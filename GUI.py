d903bf7a041246c9909fcda55a476ff7535f295a
DohkoH <agumonxdx@gmail.com>
2021-10-11 12:22:51 -0500

    Botones Principales

--- GUI.py
+++ GUI.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 
-
 Ventana1=tk.Tk()
 Ventana1.geometry("600x600")
+Ventana1.resizable(0,0)
 Ventana1.title("Control por Vision Artificial")
 
 
+BotonInicio=tk.Button(text="Iniciar")
+BotonInicio.place(x=250,y=250)
 
 
+BotonFin=tk.Button(text="Detener")
+BotonFin.place(x=300,y=250)
 
 Ventana1.mainloop()
